75ac28e63fedd960bd7d8e41b8c404231c447602
Yuanleoluo <leoyuanluo@gmail.com>
2017-11-06 19:57:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -115,4 +115,20 @@ def winner(board)
   end
 end
 
+def turn_count(board)
+  count = 0
+  board.each do |x|
+    if x === "O" || x === "X"
+      count += 1
+    end
+  end
+  count
+end
 
+def current_player(board)
+  if turn_count(board) % 2 === 0
+    "X"
+  else
+    "O"
+  end
+end
